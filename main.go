7f4c66c5016ed5ec99844d6bd922164ca81d0978
chanhyun <stephano2037@gmail.com>
2021-05-27 18:23:43 +0900

    re #2 time -8m function arguments tutorial

--- main.go
+++ main.go
@@ -11,24 +11,42 @@ add something package (21.05.23)
 learning const variable & nomral variable (21.05.23)
 
 add comment for github third (21.05.23)
+
+
+21.05.27 Functions part One
 */
 
 package main //package name (Necessary)
 import (
-	"chanhyun/something"
 	"fmt"
+	"strings"
 )
+
+//func multiply(a int, b int) int { // a type, b type, result type (total 3 things)
+func multiply(a, b int) int { // a, b type both int, result type (total 3 things)
+	return a * b
+}
+
+//func functionName(parameter) (return value,...)
+func lenAndUpper(name string) (int, string) {
+
+	return len(name), strings.ToUpper(name)
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
 
 //var name bool = false
 func main() {
-	const name = "stephano" //
-	//name = "cool"
 
-	//	var names string = "nico"
-	names := "nico" //find appropriate type itself ,축약형 대입
-	//	names = "lynn" //can change the variable content
-	fmt.Println(name)
+	//If you don't use your variable in Go, then compiler makes error
+	//totalLength, _ := lenAndUpper("stephano") //ignore value with '_'
+	//totalLength, uppperName := lenAndUpper("stephano")
+	//fmt.Println(totalLength, uppperName)
+	//fmt.Println(totalLength)
+	//fmt.Println(multiply(2, 2))
 
-	fmt.Println(names)
-	something.SayHello()
+	repeatMe("nico", "stephano", "dal", "marcy")
+
 }
