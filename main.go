ad17966b81b0dfe012d572a2b3fc08bf6693f1dd
chanhyun <stephano2037@gmail.com>
2021-05-29 08:39:08 +0900

    re #10 time -3m if-else in GO

--- main.go
+++ main.go
@@ -20,65 +20,34 @@ add comment for github third (21.05.23)
 21.05.27 for loop
 - range
 
+
+21.05.29 if else
+
 */
 
 package main //package name (Necessary)
 import (
 	"fmt"
-	"strings"
 )
 
-//func multiply(a int, b int) int { // a type, b type, result type (total 3 things)
-func multiply(a, b int) int { // a, b type both int, result type (total 3 things)
-	return a * b
-}
+func canIDrink(age int) bool {
+	//koreanAge := age + 2
 
-//func functionName(parameter) (return value,...)
-// func lenAndUpper(name string) (int, string) {
+	//if koreanAge < 18
+	//can add variable in if
+	if koreanAge := age + 2; koreanAge < 18 {
+		return false
+	} else {
 
-// 	return len(name), strings.ToUpper(name)
-// }
-
-//Already return variable is setting (like parameter setting)
-//Naked return
-func lenAndUpper(name string) (length int, uppercase string) {
-	defer fmt.Println("I'm done") // After function close
-	length = len(name)
-	uppercase = strings.ToUpper(name)
-	return
-
-}
-
-func repeatMe(words ...string) {
-	fmt.Println(words)
-}
-
-func superAdd(numbers ...int) int {
-
-	//range give index
-	// for index, number := range numbers {
-	// 	fmt.Println(index, number)
-	// }
-	//
-
-	//:= declared
-	// for i := 0; i < len(numbers); i++ {
-	// 	fmt.Println(numbers[i])
-	// }
-
-	total := 0
-	for _, number := range numbers { //only get value, ignore index
-		total += number //total = total + number
 	}
-	defer fmt.Println((total))
-	return total
-}
+
+	return true
+
+} //end of canIdrinkk function
 
 //var name bool = false
 func main() {
-	total := superAdd(1, 2, 3, 4, 5, 8)
-
-	fmt.Println("Total: ", total)
+	fmt.Println(canIDrink(16))
 	//for loop
 
 }
