20f4fe30d4052440d96e9703c75a66505dc5e41f
chanhyun <stephano2037@gmail.com>
2021-06-07 10:50:16 +0900

    re #12 time-8m hiturl with detail error msg

--- main.go
+++ main.go
@@ -39,7 +39,8 @@ add comment for github third (21.05.23)
 
 
 21.06.07  [URL checker project]
-
+- url check (url hit)
+- using map
 */
 
 package main
@@ -53,24 +54,48 @@ import (
 var errRequestedFailed = errors.New("requested error")
 
 func main() {
+
+	//Cause Error , but compiler doesn't know about this
+	//var results map[string]string
+
+	//var results := map[string]string{}
+
+	//make is making map with empty
+	var results = make(map[string]string) //nil
+
 	urls := []string{"https://www.airbnb.com/",
 		"https://www.google.com/",
 		"https://www.amazon.com/"}
 
+	results["bye"] = "hello"
 	//index, value
 	for _, url := range urls {
+		result := "ok"
 		//fmt.Println(url)
-		hitURL(url)
+		err := hitURL(url)
+		if err != nil {
+			result = "failed"
+		}
+		results[url] = result
 	}
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
+
+	//fmt.Println(results)
 
 } //end of main
 
 func hitURL(url string) error {
 	fmt.Println("Checking:", url)
 	resp, err := http.Get(url)
-	if err == nil || resp.StatusCode >= 400 {
+	if err != nil || resp.StatusCode >= 400 {
+		//if i want to see about each site error
+		fmt.Println(err, resp.StatusCode)
 		return errRequestedFailed
 	}
+	//401 : unauthorized
+	//429 : too many requests
 	//100,200,300 -> redirection, 400 series-> error
 	return nil
 }
